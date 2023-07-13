ef95a56c1ab6469ab0701e0b6b9336868bd49159
Kitwana Akil <kakil@me.com>
2023-07-13 08:50:18 -0400

    Initial Commit

---
+++ SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by Kitwana Akil on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
